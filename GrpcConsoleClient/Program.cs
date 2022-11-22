5648dec6588260b18eb8c07c070a4a939a6c753e
xamar-sharp <xamacoredevelopment@gmail.com>
2022-11-22 19:33:09 +0300

    Main edited

--- GrpcConsoleClient/Program.cs
+++ GrpcConsoleClient/Program.cs
@@ -6,7 +6,11 @@ namespace GrpcConsoleClient
     {
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-           
+            ClientFacade facade = new ClientFacade("https://localhost:5001/");
+            await facade.ReadFileFromServer();
+            await facade.RequestClientUsageDelta();
+            await facade.RequestServerUsage();
+            await facade.StartTelephony();
         }
     }
 }
